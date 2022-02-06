bb3632a023b808476936a64ba26be19e99cb6c8e
Konstantin <syancheg@gmail.com>
2022-02-06 16:51:33 +0300

    add_admin_lte

---
+++ resources/views/admin/main/index.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layouts.main')
+
+@section('content')
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+    </div><!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+@endsection
